369fe309f11167ab7d7cabbad86bff23a7055c78
zhwir <zhwir@163.com>
2024-09-06 16:56:32 +0800

    fix require

---
+++ index.js
@@ -0,0 +1,13 @@
+const sdkContractsMgr = require("./contracts-mgr.js");
+const sdkContracts = require("./contracts.js");
+const sdkOgmiosUtils = require("./ogmios-utils2.js");
+const sdkSdk = require("./sdk.js");
+const sdkUtils = require("./utils.js");
+
+module.exports = {
+  sdkContractsMgr,
+  sdkContracts,
+  sdkOgmiosUtils,
+  sdkSdk,
+  sdkUtils
+};
