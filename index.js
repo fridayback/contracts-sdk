3af70e6fa511063a79a5facbc15612cda8561d9b
liulin <blue-sky-dl5@163.com>
2025-08-14 15:22:40 +0800

    release 1.1

---
+++ index.js
@@ -0,0 +1,11 @@
+/*
+ * @Author: liulin blue-sky-dl5@163.com
+ * @Date: 2025-08-14 11:31:12
+ * @LastEditors: liulin blue-sky-dl5@163.com
+ * @LastEditTime: 2025-08-14 15:20:35
+ * @FilePath: /contracts-sdk/index.js
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ */
+module.exports.ContractSdk = require('./sdk');
+module.exports.contracts = require('./contracts');
+module.exports.contracts_mgr = require('./contracts-mgr');
